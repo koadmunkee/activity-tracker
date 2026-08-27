ed267ec2c12abf002743dca38efed309ed13d96c
George Ruiz <george.ruiz.iii@gmail.com>
2026-08-27 09:57:56 -0700

    Enable cloud function scheduler to be redeployed.

--- etl/deploy-scheduler.sh
+++ etl/deploy-scheduler.sh
@@ -7,16 +7,16 @@ export SERVICE_ACCOUNT="activity-tracker-etl-invoker@${PROJECT_ID}.iam.gservicea
 export FUNCTION_NAME="sync_blood_glucose"
 export REGION="us-west2"
 
-# Grant the invoker role on the function
 gcloud functions add-invoker-policy-binding ${FUNCTION_NAME} \
   --region=${REGION} \
   --member="serviceAccount:${SERVICE_ACCOUNT}"
 
-#gcloud scheduler jobs create http activity-tracker-etl-trigger \
-#  --location="${REGION}" \
-#  --schedule="0 10 * * *" \
-#  --time-zone="UTC" \
-#  --uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION_NAME}" \
-#  --http-method=POST \
-#  --oidc-service-account-email="${SERVICE_ACCOUNT}" \
-#  --oidc-token-audience="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION_NAME}"
+# Replace 'update' with 'create' for initial deployment
+gcloud scheduler jobs update http activity-tracker-etl-trigger \
+  --location="${REGION}" \
+  --schedule="0 12 * * *" \
+  --time-zone="UTC" \
+  --uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION_NAME}" \
+  --http-method=POST \
+  --oidc-service-account-email="${SERVICE_ACCOUNT}" \
+  --oidc-token-audience="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION_NAME}"
